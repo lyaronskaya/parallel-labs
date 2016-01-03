652341348c5691d2e4385084a2d70d1a8daecc6c
lyaronskaya <luba.yaronskaya@gmail.com>
2016-01-03 15:01:39 +0300

    _

--- 5sem_openmp2/main.cpp
+++ 5sem_openmp2/main.cpp
@@ -171,10 +171,10 @@ struct StartHandler : public Handler {
             {
                 WorkerArg* arg;
                 int id = omp_get_thread_num();
-                arg->first = id * per_thread;
+//                arg->first = id * per_thread;
 //                arg->last = arg->first + per_thread;
 //                arg->id = id;
-                cout << id << endl;
+                cout << per_thread << endl;
 //                worker_func(arg);
             }
         }
