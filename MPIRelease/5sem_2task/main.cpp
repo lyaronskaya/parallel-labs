f2b82451617d5613de19485485ba8723f6f18121
lyaronskaya <luba.yaronskaya@gmail.com>
2016-01-04 19:16:52 +0300

    _

--- MPIRelease/5sem_2task/main.cpp
+++ MPIRelease/5sem_2task/main.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include "mpi.h"
 #include "MPILifeSolver.h"
-//#include <mpi.h>
+
 #define mymessage 99
-//
+
 using namespace std;
-//
+
 int main(int argc, char** argv) {
     MPI_Status status;
     MPI_Init(&argc, &argv);
@@ -20,31 +20,29 @@ int main(int argc, char** argv) {
     if (size < 2) {
         throw "Very few workers are available.";
     }
- 
-    int i= (rank+1)*5;
-    cout << i << endl;
-    bool* buf = new bool[2];
-//    printf("\nBefore sending: i= %d on %d\n", i, rank);
-    if (rank==0) {
-//        cout << i + 10 << "\n";
-        cin >> i;
-        cerr << "end" << endl;
-        MPI_Send(&i, 1, MPI_INT, 1, mymessage, MPI_COMM_WORLD);
+    
+    if (rank == 0) {
+        MPILifeSolver solver;
+        solver.run(size);
+    } else {
+        Worker worker;
+        worker.worker_function(rank, size);
     }
-    if (rank==1) {
-        int j;
-//        cerr << "start" << endl;
-        cin >> j;
+//
+////    printf("\nBefore sending: i= %d on %d\n", i, rank);
+//    if (rank==0) {
+////        cout << i + 10 << "\n";
+//        cin >> i;
 //        cerr << "end" << endl;
-//        cout << "j" << j << "\n";
-        MPI_Recv(buf,1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    }
-//    if (rank == 0) {
-//        MPILifeSolver solver;
-//        solver.run(size);
-//    } else {
-//        Worker worker;
-//        worker.worker_function(rank, size);
+//        MPI_Send(&i, 1, MPI_INT, 1, mymessage, MPI_COMM_WORLD);
+//    }
+//    if (rank==1) {
+//        int j;
+////        cerr << "start" << endl;
+//        cin >> j;
+////        cerr << "end" << endl;
+////        cout << "j" << j << "\n";
+//        MPI_Recv(buf,1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 //    }
     
     MPI_Finalize();
